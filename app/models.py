e7aeb0fd98517f9bd90c44097dbcd3c898b91eb8
Emre Kazan <emre.kazan@plentific.com>
2015-05-18 19:49:39 +0100

    Part 5, user logins, completed.

--- app/models.py
+++ app/models.py
@@ -6,6 +6,18 @@ class User(db.Model):
 	email = db.Column(db.String(120), primary_key=False, unique=True)
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+	def is_authenticated(self):
+		return True
+
+	def is_active(self):
+		return True
+
+	def is_anonymous(self):
+		return False
+
+	def get_id(self):
+		return str(self.id) # Python 3
+
 	def __repr__(self):
 		return '<User {}'.format(self.nickname)
 
--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-from microblog import app
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+from app import app, db
 
-app.run(debug=True)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+from app import models
+
+if __name__ == '__main__':
+	# app.run(debug=True)
+	app.debug = True
+	manager.run()
