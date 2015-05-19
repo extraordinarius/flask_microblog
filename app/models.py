480e114213566d94763ee7c3cf0f874765449574
Emre Kazan <emre.kazan@plentific.com>
2015-05-19 23:15:43 +0100

    Part 6 completed

--- app/models.py
+++ app/models.py
@@ -1,10 +1,20 @@
 from app import db
+from hashlib import md5
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	nickname = db.Column(db.Integer, primary_key=False, unique=True)
 	email = db.Column(db.String(120), primary_key=False, unique=True)
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
+	about_me = db.Column(db.String(140))
+	last_seen = db.Column(db.DateTime)
+
+	def __init__(self, nickname, email, posts, about_me, last_seen):
+		self.nickname = nickname
+		self.email = email
+		self.posts = posts
+		self.about_me = about_me
+		self.last_seen = last_seen
 
 	def is_authenticated(self):
 		return True
@@ -18,6 +28,10 @@ class User(db.Model):
 	def get_id(self):
 		return str(self.id) # Python 3
 
+	def avatar(self, size):
+		return 'http://www.gravatar.com/avatar/{}?d=mm&s={}'.format(
+					md5(self.email.encode('utf-8')).hexdigest(), size)
+
 	def __repr__(self):
 		return '<User {}'.format(self.nickname)
 
@@ -26,6 +40,11 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # def __init__(self, body, timestamp, user_id):
+    # 	self.body = body
+    # 	self.timestamp = timestamp
+    # 	self.user_id = user_id
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
