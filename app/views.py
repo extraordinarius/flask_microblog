1627132970e795265adfd6ff99d4fa2cd88886b7
Emre Kazan <emre.kazan@plentific.com>
2015-05-25 00:19:26 +0100

    Part 9 completed

--- app/views.py
+++ app/views.py
@@ -3,9 +3,10 @@ from flask import render_template, flash, redirect, \
 from flask.ext.login import login_user, logout_user, \
 						current_user, login_required
 from app import app, db, lm, oid
-from .forms import LoginForm, EditForm
-from .models import User
+from .forms import LoginForm, EditForm, PostForm
+from .models import User, Post
 from datetime import datetime
+from config import POSTS_PER_PAGE
 
 @lm.user_loader
 def load_user(id):
@@ -19,25 +20,26 @@ def before_request():
 		db.session.add(g.user)
 		db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index/<int:page>', methods=['GET', 'POST'])
 @login_required
-def index():
-	user = {'nickname': 'Emre'}  # fake user
-	posts = [
-		{
-			'author': {'nickname': 'John'},
-			'body': 'Beautiful day in Portland!'
-		},
-		{
-			'author': {'nickname': 'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		}
-	]
+def index(page=1):
+	form = PostForm()
+	if form.validate_on_submit():
+		post = Post(body=form.post.data, 
+					timestamp=datetime.utcnow(), 
+					author=g.user)
+		db.session.add(post)
+		db.session.commit()
+		flash('Your post is now live!')
+		return redirect(url_for('index'))
+
+	posts = g.user.followed_posts().paginate(page, POSTS_PER_PAGE, False)
 
 	return render_template('index.html',
 							title='Home',
-							user=user,
+							form=form,
 							posts=posts)
 
 
@@ -119,17 +121,14 @@ def logout():
 
 @app.route('/user/<nickname>')
 @login_required
-def user(nickname):
+def user(nickname, page=1):
 	user = User.query.filter_by(nickname=nickname).first()
 	
 	if user == None:
 		flash('User {} not found.'.format(nickname))
 		return redirect(url_for('index'))
 	
-	posts = [
-		{'author': user, 'body': 'Test post #1'},
-		{'author': user, 'body': 'Test post #2'}
-	]
+	posts = user.posts.paginate(page, POSTS_PER_PAGE, False)
 
 	return render_template('user.html',
 							user=user,
--- config.py
+++ config.py
@@ -7,6 +7,9 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+# pagination
+POSTS_PER_PAGE = 3
+
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
