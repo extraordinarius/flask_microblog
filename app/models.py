cadc40d863d2f2dc5d5f6044b1cf8d4419466dca
Emre Kazan <emre.kazan@plentific.com>
2015-05-25 13:27:24 +0100

    Part 10 completed, whoosh doesn't work with Py3

--- app/models.py
+++ app/models.py
@@ -1,5 +1,16 @@
-from app import db
+from app import app, db
 from hashlib import md5
+import sys
+
+# In case you want enable and disable the search feature
+# based on Python version
+if sys.version_info >= (3, 0):
+	enable_search = False
+else:
+	enable_search = True
+	from flask.ext.whooshalchemy import whooshalchemy
+# enable_search = True
+# from flask.ext.whooshalchemy import whooshalchemy
 
 followers = db.Table('followers',
 	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -81,11 +92,15 @@ class User(db.Model):
 		return '<User {}'.format(self.nickname)
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	__searchable__ = ['body']
 
-    def __repr__(self):
-        return '<Post %r>' % (self.body)
-	
+	id = db.Column(db.Integer, primary_key = True)
+	body = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+	def __repr__(self):
+		return '<Post %r>' % (self.body)
+
+if enable_search:
+	whooshalchemy.whoosh_index(app, Post)
